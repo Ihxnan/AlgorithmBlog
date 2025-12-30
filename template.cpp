d78f948b82f05c9d76d0bfe0bcb88acee2a40e53
Ihxnan <1377537238@qq.com>
2025-12-30 12:28:07 +0800

    feat/dp: add a template.cpp file

---
+++ template.cpp
@@ -0,0 +1,133 @@
+#include <algorithm>
+#include <bitset>
+#include <cmath>
+#include <cstring>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+// #define int long long
+#define AUTHOR "Ihxnan"
+#define all(x) x.begin(), x.end()
+#define out(x) cout << (x) << endl
+#define out1(x) cout << #x << " = " << (x) << endl
+#define out2(x, y) cout << #x << " = " << (x) << ", " << #y << " = " << (y) << endl
+#define out3(x, y, z) cout << #x << " = " << (x) << ", " << #y << " = " << (y) << ", " << #z << " = " << (z) << endl
+#define ls(x) ((x) << 1)
+#define rs(x) ((x) << 1 | 1)
+#define lowbit(x) ((x) & -(x))
+#define yn(ans) printf("%s\n", (ans) ? "Yes" : "No")
+#define YN(ans) printf("%s\n", (ans) ? "YES" : "NO")
+#define pb push_back
+#define qb pop_back
+#define pf push_front
+#define qf pop_front
+#define endl '\n'
+#define no                    \
+    do                        \
+    {                         \
+        cout << "No" << endl; \
+        return;               \
+    } while (0)
+#define yes                    \
+    do                         \
+    {                          \
+        cout << "Yes" << endl; \
+        return;                \
+    } while (0)
+
+using namespace std;
+using ll = long long;
+using lll = __int128;
+using ull = unsigned long long;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vl = vector<ll>;
+using vvl = vector<vl>;
+using vs = vector<string>;
+using pii = pair<int, int>;
+using vii = vector<pii>;
+using pll = pair<ll, ll>;
+using ti = tuple<int, int, int>;
+using tl = tuple<ll, ll, ll>;
+
+const int iINF = 0x3f3f3f3f;
+const ll lINF = 0x3f3f3f3f3f3f3f3f;
+const int mod = 998244353;
+const ll MOD = 1000000007;
+
+template <typename T1, typename T2>
+istream &operator>>(istream &in, pair<T1, T2> &obj)
+{
+    in >> obj.first >> obj.second;
+    return in;
+}
+template <typename T>
+istream &operator>>(istream &in, vector<T> &obj)
+{
+    for (auto &p : obj)
+        in >> p;
+    return in;
+}
+template <typename T>
+ostream &operator<<(ostream &out, const set<T> &obj)
+{
+    out << "set(";
+    for (auto it = obj.begin(); it != obj.end(); it++)
+        out << (it == obj.begin() ? "" : ", ") << *it;
+    out << ")";
+    return out;
+}
+template <typename T1, typename T2>
+ostream &operator<<(ostream &out, const map<T1, T2> &obj)
+{
+    out << "map(";
+    for (auto it = obj.begin(); it != obj.end(); it++)
+        out << (it == obj.begin() ? "" : ", ") << it->first << ": " << it->second;
+    out << ")";
+    return out;
+}
+template <typename T1, typename T2>
+ostream &operator<<(ostream &out, const pair<T1, T2> &obj)
+{
+    out << "<" << obj.first << ", " << obj.second << ">";
+    return out;
+}
+template <typename T>
+ostream &operator<<(ostream &out, const vector<T> &obj)
+{
+    out << "vector(";
+    for (auto it = obj.begin(); it != obj.end(); it++)
+        out << (it == obj.begin() ? "" : ", ") << *it;
+    out << ")";
+    return out;
+}
+ll gcd(ll a, ll b)
+{
+    return b ? gcd(b, a % b) : a;
+}
+
+void solve()
+{
+}
+
+signed main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int t = 1;
+    cin >> t;
+    while (t--)
+        solve();
+
+    return 0;
+}
