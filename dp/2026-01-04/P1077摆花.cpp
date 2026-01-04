0676dd15ba58db456e3706b00bb88fbce5b86cff
Ihxnan <1377537238@qq.com>
2026-01-04 15:44:49 +0800

    feat/dp: add P1077

---
+++ dp/2026-01-04/P1077摆花.cpp
@@ -0,0 +1,14 @@
+// https://www.luogu.com.cn/problem/P1077
+#include "../../template.cpp"
+
+void init()
+{
+    t = 1; // 只有一组测试数据
+}
+
+void solve()
+{
+    int n, m;
+    cin >> n >> m;
+    vi arr(n + 1);
+}
